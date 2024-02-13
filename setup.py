26afac12d84e21ed389045c3de3983ffdc6e0e42
EduardoProfe <eduardoglez64377@gmail.com>
2024-02-13 00:43:43 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\\n" + fh.read()
 
+
 setup(
     name="mn-api",
     version='{{VERSION_PLACEHOLDER}}',
